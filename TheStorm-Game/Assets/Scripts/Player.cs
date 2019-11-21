4c488dea59f02907768adceb5d39ef29eb1eb7ec
Chris Walsh <cpw564@utulsa.edu>
2019-11-21 14:58:28 -0600

    Reorganizing Scripts and Added subclass example

---
+++ TheStorm-Game/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float health;
+    public float training;
+    public float stealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector2(transform.position.x + (h * speed),
+           transform.position.y + (v * speed));
+    }
+}
