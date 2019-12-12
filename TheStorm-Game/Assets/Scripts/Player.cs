97bcf49fc9ebb4483402b7e80f23309d58a27836
Morgandunn21 <morgandunn21@gmail.com>
2019-12-11 22:34:41 -0600

    Finished merging player script

--- TheStorm-Game/Assets/Scripts/Player.cs
+++ TheStorm-Game/Assets/Scripts/Player.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    public float speed;
+    public GameObject secondaryWeapon;
+    [Header("Player Attributes")]
     public float rotationSpeed;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +18,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartAttack();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            StopAttack();
+        }
+    }
+
+    protected override void Move()
+    {
+        Rotate();
+        // PLAYER MOVEMENT //
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        //print(h);
+        Vector3 movement = new Vector3(h, 0, v);
+        movement.Normalize();
+        movement *= (speed * Time.deltaTime);
 
-        gameObject.transform.position += new Vector3(h, 0, v).normalized * speed;
+        transform.Translate(movement);
+    }
 
+    private void Rotate()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit = new RaycastHit();
         if (Physics.Raycast(ray, out hit, 100))
