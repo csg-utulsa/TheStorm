96674269cf5bc91c3c92bcb558fae890655964b4
Matthew King <crashpro33@gmail.com>
2019-12-17 20:58:55 -0600

    Final Guard Scripts
    Created a new branch, pulled from master, then updated the scripts "alertEnemy" and "Enemy" to match the completed scripts from GuardMechanics. Should solve conflicts

--- TheStorm-Game/Assets/Scripts/EnemyGuard/Enemy.cs
+++ TheStorm-Game/Assets/Scripts/EnemyGuard/Enemy.cs
@@ -2,32 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
-public class Enemy : Character
+public class Enemy : MonoBehaviour
 {
+    // GAME OBJECTS //
     [Header("GameObjects")]
     public GameObject player;
-    public GameObject FOV_p;
+    public GameObject pivot;
     public GameObject FOV;
-    public GameObject agroCircle;
-    
+    public GameObject aggroCircle;
+    public Slider healthBar;
+
+    // MATERIALS //
     [Header("Materials")]
     public Material m_yellow;
     public Material m_red;
+
+    // VARIABLES
     [Header("Variables")]
+    public float speed;
     public bool alerted = false;
+
+    // AI PATHING //
     [Header("Pathing")]
+    public NavMeshAgent agent;
     public Transform[] waypoints;
     private int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        // find the player
         player = GameObject.FindGameObjectWithTag("Player");
 
+        // get the attached navmeshagent component
+        agent = GetComponent<NavMeshAgent>();
+
+        // prevent the gameobject from rotating by means of the agent
+        agent.updateRotation = false;
+
+        // if there are waypoints
         if (waypoints.Length > 0)
         {
-            transform.position = waypoints[waypointIndex].transform.position;
+            // set the destination to walk towards
+            agent.SetDestination(waypoints[waypointIndex].transform.position);
         }
     }
 
@@ -35,60 +54,44 @@ public class Enemy : Character
     void Update()
     {
         // DEBUG TO TEST DAMAGE FUNCTION //
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(5);
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    takeDamage(5);
+        //}
 
-        Move();
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        print("OnCollisionEntered");
-        if (collision.transform.gameObject.tag == "Player" || collision.transform.gameObject.tag == "Enemy")
-        {
-            BecomeAlerted();
-        }
-    }
-
-    public void BecomeAlerted()
-    {
-        print("guard triggered");
-        alerted = true;
-        FOV.SetActive(false);
-        agroCircle.SetActive(true);
-    }
-
-    protected override void Move()
-    {
         // PASSIVE & FOLLOWING WAYPOINTS //
         if (!alerted && waypoints != null)
         {
             if (waypointIndex <= waypoints.Length - 1)
             {
                 // distance between waypoint and enemy
-                Vector3 distance = waypoints[waypointIndex].position - FOV_p.transform.position;
+                Vector3 distance = waypoints[waypointIndex].position - pivot.transform.position;
 
                 // calculate rotation between field of vision and the waypoint position
-                Quaternion rot = Quaternion.Slerp(FOV_p.transform.rotation, Quaternion.LookRotation(distance), speed * Time.deltaTime);
+                Quaternion rot = Quaternion.Slerp(pivot.transform.rotation, Quaternion.LookRotation(distance), speed * Time.deltaTime);
 
                 // rotate the field of vision
-                FOV_p.transform.rotation = rot;
+                rot.x = 0;
+                rot.z = 0;
+                pivot.transform.rotation = rot;
 
-                // move towards the waypoint
-                transform.position = Vector3.MoveTowards(transform.position,
-                    waypoints[waypointIndex].transform.position,
-                    speed * Time.deltaTime);
+                // set the new destination to walk towards
+                agent.SetDestination(waypoints[waypointIndex].transform.position);
 
-                // goto next waypoint or loop movement around waypoints upon reaching the last one
-                if (transform.position == waypoints[waypointIndex].transform.position)
+                // if the agent's x and z match the x and z of the waypoint
+                if (agent.transform.position.x == waypoints[waypointIndex].transform.position.x &&
+                    agent.transform.position.z == waypoints[waypointIndex].transform.position.z)
                 {
+                    // debug for when the player reached the waypoint
+                    print("Waypoint reached");
+
+                    // increment the waypoint index
                     waypointIndex += 1;
                 }
             }
             else
             {
+                // reset the pathing to the origin
                 waypointIndex = 0;
             }
         }
@@ -96,7 +99,55 @@ public class Enemy : Character
         // AGGRO //
         if (alerted)
         {
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            // update the agent's destination as the position of the player
+            agent.SetDestination(player.transform.position);
+        }
+    }
+
+    // HITBOX COLLISION //
+    private void OnCollisionEnter(Collision collision)
+    {
+        // print the function call
+        print("OnCollisionEnter(" + collision.gameObject + ")");
+
+        // if collided with by either a player or other enemy
+        if (collision.transform.gameObject.tag == "Player" || collision.transform.gameObject.tag == "Enemy")
+        {
+            // function call
+            becomeAlerted();
+        }
+    }
+
+    // ALERT //
+    public void becomeAlerted()
+    {
+        // print the function call
+        print("becomeAlerted()");
+
+        // set status to alerted
+        alerted = true;
+
+        // disable field of vision
+        FOV.SetActive(false);
+
+        // set active the aggro circle
+        aggroCircle.SetActive(true);
+    }
+
+    // TAKE DAMAGE //
+    public void takeDamage(int damage)
+    {
+        // print the function call
+        print("takeDamage(" + damage + ")");
+
+        // reduce the health bar value by the damage amount
+        healthBar.value -= damage;
+
+        // if the health has reached 0
+        if (healthBar.value <= 0)
+        {
+            // destroy the game object
+            Destroy(gameObject);
         }
     }
 }
--- TheStorm-Game/Assets/Scripts/EnemyGuard/alertEnemy.cs
+++ TheStorm-Game/Assets/Scripts/EnemyGuard/alertEnemy.cs
@@ -2,23 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// LAST UPDATED DEC 17 2019
+
 public class alertEnemy : MonoBehaviour
 {
-    public GameObject parent;
-    Enemy enemy;
+    // SCRIPTS //
+    public Enemy enemy;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemy = GetComponentInParent<Transform>().parent.gameObject.GetComponentInParent<Enemy>();
-        //enemy = parent.GetComponent<Enemy>();
+        // Grab the script from the grandparent
+        enemy = GetComponentInParent<Transform>().parent.GetComponentInParent<Enemy>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // if collided with by the player or other enemy
         if (other.tag == "Player" || other.tag == "Enemy")
         {
-            enemy.BecomeAlerted();
+            // call the function of the enemy this object belongs to
+            enemy.becomeAlerted();
         }
     }
 }
