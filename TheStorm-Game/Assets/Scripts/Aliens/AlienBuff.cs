ca4641447234531641043973d39f1583c4219eef
Tabor Kvasnicka <kamakwazee@gmail.com>
2020-01-30 16:13:31 -0600

    Added Base Alien Classes

---
+++ TheStorm-Game/Assets/Scripts/Aliens/AlienBuff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// AlienBuff is the base class for managing the buffs
+/// given by Aliens.
+/// </summary>
+public class AlienBuff : MonoBehaviour
+{
+    
+    /// <summary>
+    /// Applies the buff to the player.
+    /// </summary>
+    public void ApplyBuff()
+    {
+
+        Debug.Log("Applying buff");
+
+    }
+
+}
