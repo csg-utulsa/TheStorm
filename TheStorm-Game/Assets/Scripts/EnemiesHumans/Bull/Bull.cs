2eafd47eb85b31aefa11e20126825e1dac33d508
Tabor Kvasnicka <kamakwazee@gmail.com>
2020-02-13 16:22:55 -0600

    Added comments to Bull

--- TheStorm-Game/Assets/Scripts/EnemiesHumans/Bull/Bull.cs
+++ TheStorm-Game/Assets/Scripts/EnemiesHumans/Bull/Bull.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Handles the Bull Guard
+/// </summary>
 public class Bull : Enemy
 {
     
+    /*
+     * Currently the Bull does not have special functionality implemented.
+     * It just has different stats.
+     */
+
 }
