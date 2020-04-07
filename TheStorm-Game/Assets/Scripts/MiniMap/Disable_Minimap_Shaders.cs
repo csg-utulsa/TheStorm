bc100f6547d62af644fbaf24353e261bbb924728
app353 <32683325+app353@users.noreply.github.com>
2020-04-07 14:42:01 -0500

    added a basic script for player and enemy models for minimap purposes
    note the minimap script is non functional at the time. The minimap spheres are going to have wierd lighting effects until i can figure out how to disable light rendering on the minimap camera. Also another important note if there are any other layers other than default then the culling masks inside each script will require modification.

---
+++ TheStorm-Game/Assets/Scripts/MiniMap/Disable_Minimap_Shaders.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disable_Minimap_Shaders : MonoBehaviour
+{
+    private GameObject miniCamera;
+    private Shader nullShader;
+    void Start()
+    {
+        nullShader = null;
+        miniCamera = GameObject.FindGameObjectWithTag("MiniCamera");
+        miniCamera.GetComponent<Camera>().RenderWithShader(nullShader, "nullShader");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TheStorm-Game/Assets/Scripts/MiniMap/MiniMap_Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap_Enemy : MonoBehaviour
+{
+    private GameObject sphere;
+    private GameObject enemy;
+    private GameObject mainCamera;
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        //this will need to be changed in the final build
+        enemy = GameObject.FindGameObjectWithTag("Enemy_Test_Tag");
+        
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = enemy.transform.position;
+        sphere.GetComponent<Collider>().isTrigger = true;
+        sphere.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        sphere.transform.localScale = new Vector3(2, 2, 2);
+        sphere.GetComponent<Renderer>().receiveShadows = false;
+        sphere.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        
+
+        // this sets the sphere at layer 10 and culling Mask enables the default layer only
+        // if more layers need to be activated the culling mask needs to be changed
+        //use https://answers.unity.com/questions/8715/how-do-i-use-layermasks.html for culling mask layer enabling and disabling
+        sphere.layer = 10;
+        mainCamera.GetComponent<Camera>().cullingMask = 1 << 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sphere.transform.position = enemy.transform.position;
+    }
+}
