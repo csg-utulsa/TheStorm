6354cac6a255fc90a04bdf3a2818976d17cd3daf
Tabor Kvasnicka <ttk164@utulsa.edu>
2020-04-14 20:57:43 -0500

    Readded new Input System

--- TheStorm-Game/Assets/Scripts/Inventory/Interactable.cs
+++ TheStorm-Game/Assets/Scripts/Inventory/Interactable.cs
@@ -10,7 +10,9 @@ public class Interactable : MonoBehaviour
     public Transform interactionTransform; //tranform of Interactable (does not have to be the object, but an empty object defines the exact location of where the inateraction takes place)
     public bool isFocused = false; //is interactable selected
     public GameObject player; //the player
+    public Player playerScript;
     public bool hasInteracted = false;//check if interacted with
+    private bool informed = false;
 
     private Vector3 startPosition = Vector3.zero;
 
@@ -30,23 +32,31 @@ public class Interactable : MonoBehaviour
 
         float distance = Vector3.Distance(player.transform.position, interactionTransform.position);
 
-        if (isFocused && !hasInteracted)
-        {//if focused and has not yet interacted
+        if (distance <= radius)
+        {
 
-            //get distance to player
-            //float distance = Vector3.Distance(player.position, interactionTransform.position);
-               
-                if(distance <= radius)
-            {//if distnace is within radius
-                Interact();
-                hasInteracted = true;
+            if (!informed)
+            {
+                playerScript.InformInteractableClose(distance, this);
+                informed = true;
             }
-        }
+            else
+            {
 
-        if ((distance <= radius) && Input.GetKeyDown(KeyCode.E))
-        {//if distnace is within radius
-            Interact();
-            hasInteracted = true;
+                playerScript.UpdateInteractableDistance(distance, this);
+
+            }
+
+        }
+        else
+        {
+
+            if (informed)
+            {
+                playerScript.InformInteractableNotClose(this);
+                informed = false;
+            }
+
         }
     }
 
